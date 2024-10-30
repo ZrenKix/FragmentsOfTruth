929d7e5ba44d91304cea1388bf4a0a7f78773c74
linpack12 <55255203+linpack12@users.noreply.github.com>
2024-10-30 10:08:00 +0100

    Fix

--- Assets/Scripts/Objects/FireStove.cs
+++ Assets/Scripts/Objects/FireStove.cs
@@ -41,9 +41,9 @@ public class FireStove : MonoBehaviour, IInteractable
         else
         {
             //play detective voice line that says there's something in the firestove, but the fire is in the way
-            fireStoveAS.clip = firestoveLockedVoiceLine;
-            fireStoveAS.loop = false;
-            fireStoveAS.Play();
+            fireStoveAS.clip = null;
+
+            fireStoveAS.PlayOneShot(firestoveLockedVoiceLine);
             
             return false;
         }
